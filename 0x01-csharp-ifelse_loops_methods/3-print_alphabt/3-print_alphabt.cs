ccc9fab970409265b33e59f24281e0a8bece4428
JackoDev <untaljacko@gmail.com>
2020-12-01 20:35:46 -0500

    [new] files for task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 26; i++) 
+                { 
+                    if ((i + 97) == 101 || (i + 97) == 113)
+                        continue;
+                    else
+                        Console.Write(Convert.ToChar(i + 97));
+                } 
+        }
+    }
+}
