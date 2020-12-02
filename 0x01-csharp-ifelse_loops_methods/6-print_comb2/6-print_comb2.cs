896f70196e496ae624514d49aec307e206b6b430
JackoDev <untaljacko@gmail.com>
2020-12-01 22:04:08 -0500

    [new] files for task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                j = i + 1;
+                for (; j <= 9; j++)
+                {
+                    Console.Write(i);
+                    Console.Write($"{j}, ");
+                }
+            }
+            Console.WriteLine(89);
+        }
+    }
+}
