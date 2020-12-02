2680bc28010d469b3371d137609ef4dea95077db
JackoDev <untaljacko@gmail.com>
2020-12-01 21:07:13 -0500

    [new] files for task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                string hexa = Convert.ToString(i, 16);
+                Console.Write("{0} = 0x{1}\n", i, hexa);
+            }
+        }
+    }
