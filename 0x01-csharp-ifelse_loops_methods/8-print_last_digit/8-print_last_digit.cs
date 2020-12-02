a01427a30a83e5e41f1e3ba3afaf2ff69ee26ec5
JackoDev <untaljacko@gmail.com>
2020-12-01 22:47:59 -0500

    [new] files for task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int response = number % 10;
+        if (response < 0)
+            response = response * -1;
+        Console.Write(response);
+        return response;
+    }
+}
