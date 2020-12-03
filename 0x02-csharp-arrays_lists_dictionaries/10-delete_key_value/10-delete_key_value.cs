55d142e44bb91ea88ec0aafb87332241b549029a
JackoDev <untaljacko@gmail.com>
2020-12-03 07:58:54 -0500

    [fix] task 10

--- 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -5,12 +5,7 @@ class Dictionary
 {
     public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
     {
-        if (!myDict.ContainsKey(key)){
-            myDict.Add(key, value);
-        }
-        else {
-            myDict[key] = value;
-        }
+        myDict.Remove(key);
         return myDict;
     }
 }
