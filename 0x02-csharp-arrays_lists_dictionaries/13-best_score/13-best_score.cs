bee93add40c021a4c0bf4b6929f52b1423ca466d
JackoDev <untaljacko@gmail.com>
2020-12-03 10:01:19 -0500

    [new] files for task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string nameKey = "0";
+        if (myList.Count <= 0)
+        {
+            return "None";
+        }
+
+        foreach (KeyValuePair<string, int> pair in myList)
+        {
+            int maxVal = myList.Values.Max();
+            if (pair.Value == maxVal)
+            {
+                nameKey = pair.Key;
+            }
+        }
+        return nameKey;
+    }
+}
