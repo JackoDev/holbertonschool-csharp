e68e10b9c6cf7c77c628e54bd8e286e17b7d7842
JackoDev <untaljacko@gmail.com>
2020-12-03 10:59:52 -0500

    [new] files for task 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int width = myMatrix.GetLength(0);
+        int height = myMatrix.GetLength(1);
+        int[,] newMatrix = new int[width, height];
+
+        for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    int square = myMatrix[i, j] * myMatrix[i, j];
+                    newMatrix[i, j] = square;
+                }
+            }
+        return newMatrix;
+    }
+}
