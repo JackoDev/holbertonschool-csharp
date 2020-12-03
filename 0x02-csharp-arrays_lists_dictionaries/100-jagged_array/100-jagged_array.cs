ca12ba34e789b10e40edaa6cd35e9826c577b383
JackoDev <untaljacko@gmail.com>
2020-12-02 23:56:36 -0500

    [new] files for advanced task

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [][] matrix1 = new int[][]
+        {
+            new int[] {0, 1, 2, 3},
+            new int[] {0, 1, 2, 3, 4, 5, 6},
+            new int[] {0, 1}
+        };
+        for (int i = 0; i < matrix1.Length; i++)
+        {
+            for (int j = 0; j < matrix1[i].Length; j++)
+                Console.Write("{0}{1}", matrix1[i][j], j == (matrix1[i].Length -1) ? "" : " ");
+            Console.WriteLine();
+        }
+        }
+    }
+}
