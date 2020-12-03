f539073227ab95a7a16f23c1861eb9f7eb5e41f1
JackoDev <untaljacko@gmail.com>
2020-12-03 10:18:18 -0500

    [new] files for task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 5;
+            int heigth = 5;
+            int [,] newArray = new int[width, heigth];
+
+            newArray[2, 2] = 1;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < heigth; j++)
+                {
+                    if (j < heigth - 1)
+                        Console.Write(newArray[i, j] + " ");
+                    else
+                        Console.WriteLine(newArray[i, j]);
+                }
+            }
+        }
+    }
+}
