a553a6b577fb8287fc879078fa6dfbda6336759d
JackoDev <untaljacko@gmail.com>
2020-12-02 20:05:02 -0500

    [new] files for task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if (size == 0)
+            Console.WriteLine();
+        
+        int[] array1 = new int[size];
+        for (int i = 0; i < size - 1; i++)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine(size - 1);
+        return array1;
+    }
+}
