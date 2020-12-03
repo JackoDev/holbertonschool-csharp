029f849cab61d7287face3f9fd6b52abdd6bd007
JackoDev <untaljacko@gmail.com>
2020-12-02 21:55:13 -0500

    [new] files for task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> numbers = new List<int> ();
+        for (int i = 0; i < size; i++)
+        {
+            numbers.Add(i);
+        }
+        for (int j = 0; j < numbers.Count; j++)
+        {
+            if (j < numbers.Count - 1)
+                Console.Write(numbers[j] + " ");
+            else
+                Console.WriteLine(numbers[j]);
+        }
+        return numbers;
+    }
+}
