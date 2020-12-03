cf7e3956cef31103095d82fd3220ccc1c49e144b
JackoDev <untaljacko@gmail.com>
2020-12-03 17:33:48 -0500

    [new] files for task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int suma = 0;
+        List<int> newList = new HashSet<int>(myList).ToList();
+        for (int i = 0; i < newList.Count; i++)
+        {
+            suma = suma + newList[i];
+        }
+        return suma;
+    }
+}
