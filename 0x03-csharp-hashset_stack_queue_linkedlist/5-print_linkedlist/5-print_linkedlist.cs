f1c7f891d4c200fa749f4a43451bcde438cbd473
JackoDev <untaljacko@gmail.com>
2020-12-03 22:31:24 -0500

    [new] files for task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            LinkedList<int> emptyLinked = new LinkedList<int>();
+            return emptyLinked;
+        }
+        else
+        {
+            int[] numbers = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = i;
+                Console.WriteLine(numbers[i]);
+            }
+            LinkedList<int> newLinked = new LinkedList<int>(numbers);        
+            return newLinked;
+        }
+    }
+}
