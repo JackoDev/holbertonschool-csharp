9061fac4c87f3569db7e0715f27eee7fa1b174c6
JackoDev <untaljacko@gmail.com>
2020-12-03 22:57:51 -0500

    [new] files for task 7

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> nodeToInsert = new LinkedListNode<int> (n);
+
+        myLList.AddFirst(nodeToInsert);
+        return nodeToInsert;
+    }
+}
