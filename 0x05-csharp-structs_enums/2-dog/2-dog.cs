82ff4b2584487e843cfae133c68c05650e1f30a7
JackoDev <untaljacko@gmail.com>
2020-12-14 22:58:56 -0500

    [new] files for task 2

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+struct Dog
+{   
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string nameDog, float ageDog, string ownerDog, Rating ratingDog)
+    {
+        name = nameDog;
+        age = ageDog;
+        owner = ownerDog;
+        rating = ratingDog;
+    }
+}
