3a7abc007f54cc9a2130f1e4eaaec28d8698e114
JackoDev <untaljacko@gmail.com>
2020-12-15 21:30:24 -0500

    [new] files for task 3

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> The representation of an empty class </summary>
+    class Zombie
+    {
+        /// <summary> Represents the zombie's health </summary>
+        public int health;
+
+        /// <summary> Initializes a new instance of the <see cref="Zombie"/> class</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary> To initialize a new instance of the <see cref="Zombie"/> class </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+                health = value;
+        }
+
+         /// <summary> Returns the value of health's Zombie object</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
