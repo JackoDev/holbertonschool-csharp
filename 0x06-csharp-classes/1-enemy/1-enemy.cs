ba1366e0c3ab5550bf207ba8eec431250503ed9c
JackoDev <untaljacko@gmail.com>
2020-12-15 19:43:14 -0500

    [new] files for task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> The representation of an empty class </summary>
+    class Zombie
+    {
+        /// <summary> Represents the zombie's health </summary>
+        public int health;
+
+        /// <summary> Initializes a new instance of the <see cref="Zombie"/> class</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
