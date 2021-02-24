7be18ac01304222538699fdaa9d6b0bba38e054a
JackoDev <untaljacko@gmail.com>
2021-02-23 19:31:40 -0500

    [new] tested files for task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary> A obj class </summary>
+class Obj
+{
+    /// <summary> Method for verify if object obj is a subclass </summary>
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> propertiesList = t.GetProperties();
+        IEnumerable<MethodInfo> methodsList = t.GetMethods();
+
+        Console.WriteLine(t.Name + ' ' + "Properties:");
+        foreach (PropertyInfo p in propertiesList)
+        {
+            Console.WriteLine(p.Name);
+        }
+        Console.WriteLine(t.Name + ' ' + "Methods:");
+        foreach (MethodInfo m in methodsList)
+        {
+            Console.WriteLine(m.Name);
+        }
+    }
+}
