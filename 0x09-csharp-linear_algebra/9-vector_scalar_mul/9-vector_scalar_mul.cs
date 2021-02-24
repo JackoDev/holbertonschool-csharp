9b558840af2721c0da0ee29d64fcfb9ad287f4ea
JackoDev <untaljacko@gmail.com>
2021-02-23 21:14:11 -0500

    [new] tested files for task 9

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary> Operations with matrix </summary>
+class VectorMath
+{
+    /// <summary> Method for multiply a vector and a scalar </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] notPossible = {-1};
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return notPossible;
+        }
+        for (int x = 0; x < vector.Length; x++)
+        {
+            vector[x] = scalar * vector[x];
+        }
+        return vector;
+    }
+}
