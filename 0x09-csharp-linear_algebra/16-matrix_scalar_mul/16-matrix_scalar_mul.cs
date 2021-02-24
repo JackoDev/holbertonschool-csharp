6b3de0e547496adef7da93adab01656731de20d0
JackoDev <untaljacko@gmail.com>
2021-02-24 17:32:38 -0500

    [fix] edge case for task 16

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -6,16 +6,17 @@ class MatrixMath
     /// <summary> Method for multiply a matrix and a scalar </summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        double[,] notPossible = {{-1}};
-        if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+        if (matrix.GetLength(0) > 3 || matrix.GetLength(0) < 2)
+            return new double[,] { {-1} };
+
+        if (matrix.GetLength(1) > 3 || matrix.GetLength(1) < 2)
+            return new double[,] { {-1} };
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            return notPossible;
-        }
-        for (int x = 0; x < matrix.GetLength(0); x++)
-        {
-            for (int y = 0; y < matrix.GetLength(1); y++)
+            for (int j = 0; j < matrix.GetLength(0); j++)
             {
-                matrix[x, y] = matrix[x, y] * scalar;
+                matrix[i, j] *= scalar;
             }
         }
         return matrix;
