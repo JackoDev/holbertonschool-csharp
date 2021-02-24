c9a73eaea6b69c33b4a84c2e1673f3b2d1ef43e9
JackoDev <untaljacko@gmail.com>
2021-02-23 23:38:16 -0500

    [new] files for task 4

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Text
+{
+    /// <summary> A string class </summary>
+    public class Str
+    {
+        /// <summary> Returns the first unique character </summary>
+        public static int UniqueChar(string s)
+        {
+            string str1 = "";
+            string str2 = "";
+
+            if (s == "" || s == null)
+                return -1;
+
+            int rep = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                str1 = s.Substring(i, 1);
+                int indexMin = 0;
+                for (int j = 0; j < s.Length; j++)
+                {
+                    str2 = s.Substring(j, 1);
+                    if ((str1 == str2) && (i != j))
+                        continue;
+                    else if ((str1 != str2) && (i != j))
+                    {
+                        indexMin++;
+                        if (indexMin == s.Length - 1 && rep < indexMin)
+                        {
+                            rep = indexMin;
+                            return(i);
+                        }
+                    }
+                }
+            }
+            return -1; 
+        }
+    }
+}
