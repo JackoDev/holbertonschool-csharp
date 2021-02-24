9495d5122421df4ab158bc68cdc7aabaa9f02b3b
JackoDev <untaljacko@gmail.com>
2021-02-23 20:54:32 -0500

    [new] tested files for task 3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary> Operations with matrix </summary>
+class VectorMath
+{
+    /// <summary> Method for find the lenght of a vector </summary>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return -1;
+        }
+        double res = 0;
+        double vecLen = new double();
+        foreach (double x in vector)
+        {
+            double sqrt = Math.Pow(x, 2);
+            res = res + sqrt;
+        }
+        vecLen = Math.Sqrt(res);
+        return Math.Round(vecLen, 2); 
+    }
+}
