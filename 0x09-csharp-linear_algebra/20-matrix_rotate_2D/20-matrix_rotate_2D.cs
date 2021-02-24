6a45e1762c42e0a6e7e1dc00bc3340247c0cea4e
JackoDev <untaljacko@gmail.com>
2021-02-24 16:51:09 -0500

    [new] tested files for task 20

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -3,50 +3,40 @@
 /// <summary> Operations with matrix </summary>
 class MatrixMath
 {
-    /// <summary> Method for multiply two matrix </summary>
-    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
-    {
-        double[,] notPossible = {{-1}};
-        if (matrix1.GetLength(1) != matrix2.GetLength(0))
-        {
-            return notPossible;
-        }
-        int rows1 = matrix1.GetLength(0);
-        int cols1 = matrix1.GetLength(1);
-        int cols2 = matrix2.GetLength(1);
-        double[,] newMatrix = new double[rows1, cols2];
-        for (int x = 0; x < rows1; x++)
-        {
-            for (int y = 0; y < cols2; y++)
-            {
-                double n = 0; 
-                for (int z = 0; z < cols1; z++)
-                {
-                    n = n + matrix1[x, z] * matrix2[z, y];
-                }
-                newMatrix[x, y] = n;
-            }
-        }
-        return newMatrix;
-    }
-
     /// <summary> Method for rotate a matrix </summary>
     public static double[,] Rotate2D(double[,] matrix, double angle)
-    {
-        double angleCos = Math.Cos(angle);
-        double angleSin = Math.Sin(angle);
-        double[,] cosSin = {
-            {angleCos, -angleSin}, 
-            {angleSin, angleCos}
-            };
-        double[,] newMatrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
-        double[,] notPossible = {{-1}};
-
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
         {
-            return notPossible;
+            double cos = Math.Cos(angle);
+            double sin = Math.Sin(angle);
+            double[,] rotateMatrix = new double[,] {
+                {cos, sin},
+                {-sin, cos}
+            };
+
+            if (matrix is double[,] && matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+            {
+                int rowM1 = matrix.GetLength(0);
+                int colM1 = matrix.GetLength(1);
+                int colM2 = rotateMatrix.GetLength(1);
+                int rowM2 = rotateMatrix.GetLength(0);
+
+                double[,] mulMatrix = new double[rowM1, colM2];
+
+                for (int col = 0; col < colM1; col++)
+                {
+                    for (int row = 0; row < rowM1; row++)
+                    {
+                        for (int ixj = 0; ixj < colM2; ixj++)
+                        {
+                            mulMatrix[row, ixj] = Math.Round(mulMatrix[row, ixj] + matrix[row, col] * rotateMatrix[col, ixj], 2);
+                        }
+                    }
+                }
+                return mulMatrix;
+            }
+            else
+            { 
+                return new double[,]{{-1}}; 
+            }
         }
-        newMatrix = Multiply(cosSin, matrix);
-        return newMatrix;
-    }
 }
