ef9d97ca66d4b2bc492289cdb5c5270957dfc1e1
JackoDev <untaljacko@gmail.com>
2021-02-23 20:10:32 -0500

    [new] tested files for task 5

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary> A shape class </summary>
+class Shape
+{
+/// <summary> Not implemented method Area() </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
