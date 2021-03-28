2488034097e535f1bffe0abec399f366144c063b
JackoDev <untaljacko@gmail.com>
2021-03-28 10:01:00 -0500

    task 2 tested ok

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,68 @@
+using System;
+
+///<summary> Class for que opperations</summary>
+class Queue<T>
+{
+  ///<summary> Set nodes for queue </summary>
+	public class Node
+	{
+		private T value = default(T);
+		public Node next = null;
+
+		public Node(T input)
+		{
+			value = input;
+		}
+	}
+
+	public Node head;
+	public Node tail;
+	public int count;
+
+	///<summary> Add a new node at the queue's end </summary>
+	public void Enqueue(T value)
+	{
+		Node node = new Node(value);
+		if (head == null)
+		{
+			head = node;
+			tail = node;
+		}
+		else
+		{
+			tail.next = node;
+			tail = node;
+		}
+		count++;
+	}
+
+  ///<summary> Delete the last node on the Queue </summary>
+	public T Dequeue()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+		else
+		{
+			T value = head.value;
+			head = head.next;
+			count--;
+			return value;
+		}
+	}
+
+	///<summary> Returns the number of nodes in the Queue </summary>
+	public int Count()
+	{
+		return count;
+	}
+
+	///<summary> Check for the Queue's type </summary>
+	///<return> Returns the Queue's type </return>
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
+}
